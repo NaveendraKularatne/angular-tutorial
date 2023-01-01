e0421daeb179385f135a18559b629e863fa241c4
Naveendrak <naveendrak@mobitel.lk>
2023-01-01 19:49:50 +0530

    Display hard-coded data

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Employee {
+  id: string;
+  name:  string;
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
-  show() {
-    alert('Thank you for visiting us. Please send us a email on naveendrarashmika@gmail.com');
-  }
-
-  consoleCheck() {
-    console.log('Hit');
-  }
+  employees: Employee[] = [
+    {id: "1", name: "Naveendra", email: "naveendrarashmika@gmail.com", password:"123"},
+    {id: "2", name: "Rashmika", email: "rashmika@gmail.com", password:"123"},
+    {id: "3", name: "Kularatne", email: "kularatne@gmail.com", password:"123"}
+  ];
 }
